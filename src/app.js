9b3d54e292bc369ba2062d08c428e0215be0ff91
Asun Perea Ferrer <118168204+asun-pf@users.noreply.github.com>
2023-06-21 18:28:51 +0000

    I created the function getRandomElement with the parameter array that will return a random element from the given array. Then there I generate a randomIndex based on the array's length and it will return a random part of the arrays of my generateExcuse function that uses options as parameter and have the who, action, what and when variables.

--- src/app.js
+++ src/app.js
@@ -14,6 +14,42 @@ window.onload = () => {
   console.log("Hello Rigo from the console!");
 };
 
+let getRandomElement = array => {
+  let randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+let generateExcuse = options => {
+  let who = options.who || [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My pinecone"
+  ];
+  let action = options.action || ["ate", "peed", "crushed", "broke"];
+  let what = options.what || ["my homework", "the keys", "the car"];
+  let when = options.when || [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoElement = getRandomElement(who);
+  let actionElement = getRandomElement(action);
+  let whatElement = getRandomElement(what);
+  let whenElement = getRandomElement(when);
+
+  return (
+    whoElement + " " + actionElement + " " + whatElement + " " + whenElement
+  );
+};
+
+/* For the refactor to be according to what we studied together you'd have to separate this part of the code 
+into a separated function that will take any array and return the random element from it:
+ https://github.com/4GeeksAcademy/EU-11-asun-PF-readability.js/blob/main/src/app.js#L29
+
 let generateExcuse = () => {
   let who = ["The dog", "My grandma", "His turtle", "My pinecone"];
   let action = ["ate", "peed", "crushed", "broke"];
@@ -41,3 +77,5 @@ let generateExcuse = () => {
     when[whenIndex]
   );
 };
+
+*/
